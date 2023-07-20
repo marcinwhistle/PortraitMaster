8befe4ea24fe4a5031fef6f909119fd6a03a8b8b
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-07-20 15:01:27 +0200

    Add sanitize for inputs and regex for email, fix length of author and title validation

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -1,29 +1,58 @@
 const Photo = require('../models/photo.model');
+const sanitizeHtml = require('sanitize-html');
+
+// Function to validate email using regex
+const isEmailValid = (email) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
 
 /****** SUBMIT PHOTO ********/
 
 exports.add = async (req, res) => {
   try {
+    // Extract input fields
     const { title, author, email } = req.fields;
     const file = req.files.file;
 
-    if (title && author && email && file) {
+    // Perform length validation on original input
+    if (title.length >= 25) {
+      throw new Error('Title is too long');
+    }
+
+    if (author.length >= 50) {
+      throw new Error('Author name is too long');
+    }
+
+    // Sanitize input fields
+    const sanitizedTitle = sanitizeHtml(title, {
+      allowedTags: [], // Disallow all HTML tags
+      allowedAttributes: {}, // Disallow all HTML attributes
+    });
+    const sanitizedAuthor = sanitizeHtml(author, {
+      allowedTags: [], // Disallow all HTML tags
+      allowedAttributes: {}, // Disallow all HTML attributes
+    });
+    const sanitizedEmail = sanitizeHtml(email, {
+      allowedTags: [], // Disallow all HTML tags
+      allowedAttributes: {}, // Disallow all HTML attributes
+    });
+
+    if (sanitizedTitle && sanitizedAuthor && sanitizedEmail && file) {
       // if fields are not empty...
 
       const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
       const fileExt = fileName.split('.').slice(-1)[0];
-      console.log(fileExt, fileName);
-      if (
-        (fileExt[1] === 'jpg' ||
-          fileExt[1] === 'png' ||
-          fileExt[1] === 'gif') &&
-        title.length <= 25 &&
-        author.length <= 50
-      ) {
+      if (fileExt === 'jpg' || fileExt === 'png' || fileExt === 'gif') {
+        // Validate email using regex
+        if (!isEmailValid(sanitizedEmail)) {
+          throw new Error('Wrong email format');
+        }
+
         const newPhoto = new Photo({
-          title,
-          author,
-          email,
+          title: sanitizedTitle,
+          author: sanitizedAuthor,
+          email: sanitizedEmail,
           src: fileName,
           votes: 0,
         });
